040ef6dd7d8655f4158511d7e1f7fc4212b3adf9
Albert <albertzeap@gmail.com>
2023-05-09 09:55:18 -0700

    Finished login and register functionality

--- api/user.js
+++ api/user.js
@@ -1,33 +1,29 @@
-
-
-
-const URI = "http://localhost:3000/users";
 let userList = [];
+const URI = "http://localhost:3000/users"
 
 const UserApi = {
-
+    
     getUsers: () => {
         fetch(URI)
-            .then((result) => {
-                console.log("RESULT");
-                console.log(result);
-
-                return result.json();
-            })
-            .then((data) =>{
-                console.log("DATA: ");
-                console.log(data);
-
-                userList = data;  
-                console.log(userList[userList.length - 1].id);              
-            })
-            .catch((error)=>{console.log(error)});
+        .then((result) => {
+            console.log("RESULT");
+            console.log(result);
+            
+            return result.json();
+        })
+        .then((data) =>{
+            console.log("DATA: ");
+            console.log(data);
+            
+            userList = data;             
+        })
+        .catch((error)=>{console.log(error)});
     },
-
+    
     createUser: (id, fn, ln, username, password) => {
-
-            // Create the POST request
-        fetch("http://localhost:3000/users", {
+        
+        // Create the POST request
+        fetch(URI, {
             method: "POST",
             body: JSON.stringify({
                 id: id,
@@ -43,27 +39,51 @@ const UserApi = {
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-
+            
             alert("User account created!" + `\nUsername: ${data.username}`);
         });
     }
-
-
-
 }
-
 UserApi.getUsers();
 
-let regform = document.forms["regform"];
-regform.onsubmit = (e) => {
-    e.preventDefault();
+// Registration Form 
+var regform = document.forms["regform"];
+if(regform != undefined){
+    console.log(regform);
+    regform.onsubmit = (e) => {
+        e.preventDefault();
+        
+        // Collect the form data
+        let fn = document.forms["regform"]["fname"].value;
+        let ln = document.forms["regform"]["lname"].value;
+        let username = document.forms["regform"]["username"].value;
+        let password = document.forms["regform"]["password"].value;
+        
+        let id = userList[userList.length - 1].id + 1;
+        UserApi.createUser(id, fn, ln, username, password);
+    }
+}
 
-    // Collect the form data
-    let fn = document.forms["regform"]["fname"].value;
-    let ln = document.forms["regform"]["lname"].value;
-    let username = document.forms["regform"]["username"].value;
-    let password = document.forms["regform"]["password"].value;
+// Login Form 
+var logForm = document.forms["logForm"];
+let activeUser = {};
+if(logForm != undefined){
+    
+    logForm.onsubmit = (e) => {
+        e.preventDefault();
+        
+        let username = document.forms["logForm"]["username"].value;
+        let password = document.forms["logForm"]["password"].value;
+        
+        userList.forEach(user => {
+            if(user.username == username && user.password == password){
+                activeUser = user;
+                console.log(activeUser.id + " " + activeUser.first_name)
+            }
+            
+        })
+    }
+}
 
-    let id = userList[userList.length - 1].id + 1;
-    UserApi.createUser(id, fn, ln, username, password);
-} 
+
+
